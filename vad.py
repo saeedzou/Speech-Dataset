bad2d3c077c3753d20e3854cc28f0e8502bad046
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2024-10-16 15:13:57 +0330

    Add VAD class for voice activity detection

---
+++ vad.py
@@ -0,0 +1,36 @@
+import torch
+# from silero_vad import (load_silero_vad,
+#                           read_audio,
+#                           get_speech_timestamps,
+#                           collect_chunks)
+import silero_vad
+
+
+SAMPLING_RATE = 16000
+USE_ONNX = False # change this to True if you want to test onnx model
+
+class VAD:
+    def __init__(self):
+        self.model = silero_vad.load_silero_vad(onnx=USE_ONNX)
+        self.sampling_rate = SAMPLING_RATE
+        self.threshold = 0.5
+        self.min_speech_duration_ms = 250
+        self.min_silence_duration_ms = 700
+
+    def get_speech_timestamps(self, wav):
+        timestamps = silero_vad.get_speech_timestamps(wav, 
+                                       self.model, 
+                                       sampling_rate=self.sampling_rate,
+                                       threshold=self.threshold,
+                                       min_speech_duration_ms=self.min_speech_duration_ms,
+                                       min_silence_duration_ms=self.min_silence_duration_ms
+                                       )
+        return timestamps
+    
+    def collect_chunks(self, wav, timestamps, audio_path, extension='_only_speech.wav'):
+        chunks = silero_vad.collect_chunks(timestamps, wav)
+        new_path = f'{audio_path.split(".")[0]}{extension}'
+        silero_vad.save_audio(new_path, chunks, self.sampling_rate)
+        return chunks, new_path
+        
+
